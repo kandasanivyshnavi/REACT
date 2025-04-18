c12ed6c5cb94784aaac48fd945fb5a9ebbf9e235
kandasanivyshnavi <kandasanivyshnavi@gmail.com>
2025-04-18 22:24:36 +0530

    ConditionalComponent.jsx

---
+++ ConditionalComponent.jsx
@@ -0,0 +1,14 @@
+
+function App()
+{
+  const show = true;
+
+  return(
+    <div>
+      <h1>hello vyshnavi</h1>
+      {show && <div className="form"><h1>
+        suber raa bujji</h1></div>}
+    </div>
+  );
+}
+export default App;
